68cb6e61b269af085718200b080d30492c01b350
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-19 17:14:56 +0900

    feat: 유레카 구축

---
+++ src/test/java/com/eventoreureka/EventorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventoreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
