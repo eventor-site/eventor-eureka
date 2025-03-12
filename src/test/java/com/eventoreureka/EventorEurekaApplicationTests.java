b0b2e0951291779f0fd7451b7a6af1cf88471fd0
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-03-12 13:19:22 +0900

    feat: application.yml 환경 변수 암호화

--- src/test/java/com/eventoreureka/EventorEurekaApplicationTests.java
+++ src/test/java/com/eventoreureka/EventorEurekaApplicationTests.java
@@ -1,13 +1,13 @@
-// package com.eventoreureka;
-//
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest
-// class EventorEurekaApplicationTests {
-//
-// 	@Test
-// 	void contextLoads() {
-// 	}
-//
-// }
+package com.eventoreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
